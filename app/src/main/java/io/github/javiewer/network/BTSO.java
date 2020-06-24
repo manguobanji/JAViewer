90b739726bffeb81e8a7dfda5fac3edbd8e1d4a1
SeanChengN <54seancheng@gmail.com>
2020-06-24 12:58:35 +0800

    Fix BTSO.

--- app/src/main/java/io/github/javiewer/network/BTSO.java
+++ app/src/main/java/io/github/javiewer/network/BTSO.java
@@ -14,7 +14,7 @@ import retrofit2.http.Url;
  */
 public interface BTSO {
 
-    String BASE_URL = "https://btsow.club";
+    String BASE_URL = "https://btsow.fun";
     BTSO INSTANCE = new Retrofit.Builder()
             .baseUrl(BTSO.BASE_URL)
             .client(JAViewer.HTTP_CLIENT)
